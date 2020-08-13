7c4e45045108eaa8a533b225b416d9e987273171
MarlenCarmona <1718110379@utectulancingo.edu.mx>
2020-08-13 05:30:28 +0000

    PARTE 5 FUNCIONANDO

--- web/mvc/controllers/modulos/update.py
+++ web/mvc/controllers/modulos/update.py
@@ -2,18 +2,31 @@ from web.template import ALLOWED_AST_NODES
 ALLOWED_AST_NODES.append('Constant')
 import web 
 
+import web.models.personas as personas
+model_personas = personas.Personas()
+
 render = web.template.render("mvc/views/modulos/")
 
 class Update1():
-    def GET(self):
-        try:
-            return render.update() 
-        except Exception as e:
-            return "Error" + str(e.args)
-    def POST(self):
+    def GET(self, matricula):
       try:
-        form = web.input()
-        print(type(form))
-        print(form)
+        result = model_personas.view(matricula)[0]
+        return render.update(result)
       except Exception as e:
         return "Error" + str(e.args)
+    def POST(self, matricula):
+        try:
+            form = web.input()
+            id_usuario = form.id_usuario
+            nombre = form.nombre
+            primerapellido = form.primerapellido
+            segundoapellido = form.segundoapellido
+            edad = form.edad
+            fechanacimiento = form.fechanacimiento
+            genero = form.genero
+            estado = form.estado
+            model_personas.insert(id_usuario,nombre,primerapellido,segundoapellido,edad,fechanacimiento,genero,estado)
+            web.seeother('/list')
+        except Exception as e:
+            print(e)
+            return render.insert()
