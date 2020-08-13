d273c2cedf67d26097cfa2911719726cf6d2fb83
MarlenCarmona <1718110379@utectulancingo.edu.mx>
2020-08-13 04:18:52 +0000

    PARTE 5 FUNCIONANDO

--- web/mvc/controllers/modulos/delete.py
+++ web/mvc/controllers/modulos/delete.py
@@ -2,18 +2,24 @@ from web.template import ALLOWED_AST_NODES
 ALLOWED_AST_NODES.append('Constant')
 import web 
 
+import web.models.personas as personas
+model_personas = personas.Personas()
+
 render = web.template.render("mvc/views/modulos/")
 
 class Delete1():
-    def GET(self):
-        try:
-            return render.delete() 
-        except Exception as e:
-            return "Error" + str(e.args)
-    def POST(self):
+    def GET(self, matricula):
       try:
-        form = web.input()
-        print(type(form))
-        print(form)
+        result = model_personas.view(matricula)[0]
+        return render.delete(result)
       except Exception as e:
         return "Error" + str(e.args)
+    def POST(self, matricula):
+      try:
+        form = web.input()
+        matricula = form['matricula']
+        result = model_personas.delete(matricula)
+        web.seeother('/list')
+      except Exception as e:
+        print(e)
+        return "Error"
